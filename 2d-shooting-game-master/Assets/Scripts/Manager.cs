47d300fab31342b10d1d4bed5b68cc453bf5e623
Kohei Ogawa <ogawa@oysterworld.jp>
2018-11-13 18:55:05 +0900

    Unityでサンプルの作成 その2

--- 2d-shooting-game-master/Assets/Scripts/Manager.cs
+++ 2d-shooting-game-master/Assets/Scripts/Manager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
@@ -10,38 +8,34 @@ public class Manager : MonoBehaviour
     // タイトル
     private GameObject title;
 
-    void Start()
+    void Start ()
     {
         // Titleゲームオブジェクトを検索し取得する
-        title = GameObject.Find("Title");
+        title = GameObject.Find ("Title");
     }
 
-    void Update()
+    void Update ()
     {
         // ゲーム中ではなく、Xキーが押されたらtrueを返す。
-        if (IsPlaying() == false && Input.GetKeyDown(KeyCode.X))
-        {
-            GameStart();
+        if (IsPlaying () == false && Input.GetKeyDown (KeyCode.X)) {
+            GameStart ();
         }
     }
 
-    void GameStart()
+    void GameStart ()
     {
         // ゲームスタート時に、タイトルを非表示にしてプレイヤーを作成する
-        title.SetActive(false);
-        Instantiate(player, player.transform.position, player.transform.rotation);
+        title.SetActive (false);
+        Instantiate (player, player.transform.position, player.transform.rotation);
     }
 
-    public void GameOver()
+    public void GameOver ()
     {
-        // ハイスコアの保存
-        FindObjectOfType<Score>().Save();
-
         // ゲームオーバー時に、タイトルを表示する
-        title.SetActive(true);
+        title.SetActive (true);
     }
 
-    public bool IsPlaying()
+    public bool IsPlaying ()
     {
         // ゲーム中かどうかはタイトルの表示/非表示で判断する
         return title.activeSelf == false;
---
+++ 2d-shooting-game-master/Assets/Scripts/SceneLoadScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadScript : MonoBehaviour {
+
+    public void startbutton()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+}
